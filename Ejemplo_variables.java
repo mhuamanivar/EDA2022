4dadbe721654e6c9c0917cfffa87a67bf398e898
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-08 21:22:53 -0500

    Prueba de ejemplo Variables

---
+++ Ejemplo_variables.java
@@ -0,0 +1,14 @@
+public class Ejemplo_variables
+{
+	public static void main(String args[]) {
+		        
+		int edad, nacim;
+		edad = 18;
+		nacim = 2003;
+		String nombre = "Maria";
+
+		System.out.println("Su nombre es "+nombre);
+		System.out.println("Nacio en "+nacim+" y tiene "+edad+"años");
+
+	}
+}
