b157f144326ea0d2873034185d60ee28c050060e
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-09 14:10:20 -0500

    Mayus y minus de un string

---
+++ Ejemplo_strings.java
@@ -0,0 +1,13 @@
+public class Ejemplo_strings
+{
+	public static void main(String args[]) {
+		        
+		String curso = "Estructura de Datos y Algoritmos";
+
+		System.out.println("La cadena es: '"+curso+"'");
+		System.out.println("Tamaño: "+curso.length());
+		System.out.println("En mayuscula: "+curso.toUpperCase());
+		System.out.println("En minuscula: "+curso.toLowerCase());
+
+	}
+}
