ff3bb3e88eec0c214d6162371e804f9c4cfb0ad1
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-09 13:36:52 -0500

    Iniciando Ejemplo Type casting

---
+++ Ejemplo_type_casting.java
@@ -0,0 +1,13 @@
+public class Ejemplo_type_casting
+{
+	public static void main(String args[]) {
+		        
+		//Casting automatico
+		short num = 1;
+		int ent = num;
+		System.out.println("Numero de short a int: "+ent);
+		double doub = ent;
+		System.out.println("Numero de entero a double: "+doub);
+
+	}
+}
