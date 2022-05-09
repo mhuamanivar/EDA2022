2c0e52c09997779987f43e377f6867438739052c
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-09 13:44:38 -0500

    Agregando ejemplo de casting manual

--- Ejemplo_type_casting.java
+++ Ejemplo_type_casting.java
@@ -3,11 +3,19 @@ public class Ejemplo_type_casting
 	public static void main(String args[]) {
 		        
 		//Casting automatico
-		short num = 1;
-		int ent = num;
+		short sh = 1;
+		System.out.println("Numero de tipo short: "+sh);
+		int ent = sh;
 		System.out.println("Numero de short a int: "+ent);
 		double doub = ent;
-		System.out.println("Numero de entero a double: "+doub);
+		System.out.println("Numero de int a double: "+doub+"\n");
 
+		//Casting manual
+		double doub2 = 4.5;
+		System.out.println("Numero de tipo double: "+doub2);
+		int ent2 = (int)(doub2);
+		System.out.println("Numero de double a int: "+ent2);
+		short sh2 = (short)(ent2);
+		System.out.println("Numero de int a short: "+sh2);
 	}
 }
