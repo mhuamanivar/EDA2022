d00334831eae935cfc1767e145a660762c129ca5
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-09 23:55:18 -0500

    Concatenación de datos String e int

--- Ejemplo_strings.java
+++ Ejemplo_strings.java
@@ -4,10 +4,17 @@ public class Ejemplo_strings
 		        
 		String curso = "Estructura de Datos y Algoritmos";
 
-		System.out.println("La cadena es: '"+curso+"'");
-		System.out.println("Tamaño: "+curso.length());
-		System.out.println("En mayuscula: "+curso.toUpperCase());
-		System.out.println("En minuscula: "+curso.toLowerCase());
+		System.out.println("La cadena es:\t'"+curso+"'");
+		System.out.println("Tamaño:\t\t"+curso.length());
+		System.out.println("En mayuscula:\t"+curso.toUpperCase());
+		System.out.println("En minuscula:\t"+curso.toLowerCase());
+		System.out.println("Posicion de 'y':\t"+curso.indexOf("y"));
 
+		String nombre = "Melany", apell = "Huamani";
+		System.out.println("\nNombre completo: "+nombre+" "+apell);
+
+		int num1 = 10, num2 = 8;
+		System.out.println("\nSuma incorrecta de datos: "+num1+num2);
+		System.out.println("Suma correcta de datos: "+(num1+num2));
 	}
 }
