9ce4f97274a069bd12b60249232811ab0d1cd28b
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-10 00:12:16 -0500

    True, false, mayor e igualdad

---
+++ Ejemplo_booleans.java
@@ -0,0 +1,17 @@
+public class Ejemplo_booleans
+{
+	public static void main(String args[]) {
+		        
+		boolean verdad = true;
+		boolean falsedad = false;
+
+		System.out.println("La verdad se representa con: "+verdad);
+		System.out.println("La falsedad se representa con: "+falsedad);
+
+		int num1 = 10, num2 = 8;
+		System.out.println("Ana tiene "+num1+" años.\n"+
+				"María tiene "+num2+" años");
+		System.out.println("Ana es mayor que María: "+(num1 > num2));
+		System.out.println("María tiene 5 años: "+(num2 == 5));
+	}
+}
