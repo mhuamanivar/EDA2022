4afcff248a8893d85c8eb1cc40fa2869bcb8cafe
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-10 16:40:38 -0500

    Posiciones de arreglo

---
+++ Ejemplo_arrays.java
@@ -0,0 +1,12 @@
+public class Ejemplo_arrays
+{
+	public static void main(String args[]) {
+
+		String[] personas = {"Maria", "Edith", "Melsy"};
+		
+		System.out.println("En la posicion 1: "+personas[0]);
+		System.out.println("En la posicion 2: "+personas[1]);
+		System.out.println("En la posicion 3: "+personas[2]);
+
+	}
+}
