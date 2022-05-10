c6a57dbccf286e0b97e025fe3e98eea1ab3b3a7a
Edith Lucero Vilcape ROsas <evilcape@unsa.edu.pe>
2022-05-10 15:28:53 -0500

    ejemplo de operadores

---
+++ Operators.java
@@ -0,0 +1,34 @@
+
+public class Operators {
+	public static void main(String[] args) {
+		
+		
+		int x=20;
+		
+		System.out.println("x inicial:   " + x);
+		//suma
+		x+=3;
+		System.out.println("operador +=3  " + x);
+		
+		//resta
+		x-=1;
+		System.out.println("operador -=1  " + x);
+		
+		//multiplicacion
+		x*=2;
+		System.out.println("operador *=2  " + x);
+		
+		//division
+		x/=3;
+		System.out.println("operador /=3  " + x);
+		
+		//modulo
+		x%=4;
+		System.out.println("operador %=4  " + x);
+		
+		//potencia
+		x^=3;
+		System.out.println("operador ^=3  " + x);
+		
+	}
+}
