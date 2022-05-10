c8ac192f7a5b9437efdd0c9b06a0b531d5625dc3
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-10 17:52:42 -0500

    Creando metodo base para factorial

---
+++ Ejemplo_recursion.java
@@ -0,0 +1,12 @@
+public class Ejemplo_recursion
+{
+	public static void main(String args[]) {
+		int num = 9;
+		System.out.println("El numero es: "+num);
+		System.out.println("Su factorial es: "+factorial(num));
+	}
+
+	public static int factorial(int n) {
+		return 0;
+	}
+}
