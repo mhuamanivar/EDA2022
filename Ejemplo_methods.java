d6a115d1aa18d72518a3ad208a4a3cdda97877f9
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-10 17:35:53 -0500

    Ejemplo metodo finalizado

---
+++ Ejemplo_methods.java
@@ -0,0 +1,12 @@
+public class Ejemplo_methods
+{
+	public static void main(String args[]) {
+		for (int i = 0; i < 2; i++) {
+			imprimirNombre();
+		}
+	}
+
+	public static void imprimirNombre() {
+		System.out.println("Mi nombre es Melany.");
+	}
+}
