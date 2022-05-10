0f0908634f326dda1774bc84f08bafe1f5eab18d
Edith Lucero Vilcape ROsas <evilcape@unsa.edu.pe>
2022-05-10 15:23:36 -0500

    ejemplo de Data types

---
+++ Data_Types.java
@@ -0,0 +1,13 @@
+
+public class Data_Types {
+	// se declara el tipo que tienen las variables
+	//primitivos
+	int myNum = 5;               // Integer (whole number)
+	float myFloatNum = 5.99f;    // Floating point number
+	char myLetter = 'D';         // Character
+	boolean myBool = true;       // Boolean
+	double myDouble = 5.4456;
+	
+	//no primitivos
+	String myText = "Hello";     // String
+}
