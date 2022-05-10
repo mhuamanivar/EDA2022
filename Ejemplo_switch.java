154b5e8f483e33bbcd68c829bdda153f63b0f619
Melsy Melany Huamani Vargas <mhuamanivar@unsa.edu.pe>
2022-05-10 00:29:02 -0500

    Agregando switch

--- Ejemplo_switch.java
+++ Ejemplo_switch.java
@@ -2,9 +2,22 @@ public class Ejemplo_switch
 {
 	public static void main(String args[]) {
 
-		int operacion = "+";
+		String operacion = "+";
 		int n1 = 5, n2 = 7;
-		
 
+		switch(operacion) {
+			case "+":
+				System.out.println("La suma es: "+(n1+n2));
+				break;
+			case "-":
+				System.out.println("La diferencia es: "+(n1-n2));
+				break;
+			case "*":
+				System.out.println("El producto es:"+(n1*n2));
+				break;
+			case "/":
+				System.out.println("El cociente es:"+(n1/n2));
+				break;
+		}
 	}
 }
